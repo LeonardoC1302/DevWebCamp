28a7b980026eeacb8494c1533c763518c4d98f7b
Leonardo Céspedes <lecespedes@estudiantec.cr>
2024-01-09 17:31:38 -0600

    Header

---
+++ views/templates/header.php
@@ -0,0 +1,30 @@
+<header class="header">
+    <div class="header__container">
+        <nav class="header__nav">
+            <a href="/register" class="header__link">Register</a>
+            <a href="/login" class="header__link">Login</a>
+        </nav>
+
+        <div class="header__content">
+            <a href="/">
+                <h1 class="header__logo">
+                    &#60;DevWebCamp/>
+                </h1>
+            </a>
+            <p class="header__text">October 5th-6th - 2024</p>
+            <p class="header__text header__text--mode">Online - In-Person</p>
+            <a href="7register" class="header__button">Buy Pass</a>
+        </div>
+    </div>
+</header>
+<div class="bar">
+    <div class="bar__content">
+        <a href="/"><h2 class="bar__logo">&#60;DevWebCamp/></h2></a>
+        <nav class="navigation">
+            <a href="/devwebcamp" class="navigation__link">Events</a>
+            <a href="/packages" class="navigation__link">Packages</a>
+            <a href="/workshops-conferences" class="navigation__link">Workshops / Conferences</a>
+            <a href="/register" class="navigation__link">Buy Pass</a>
+        </nav>
+    </div>
+</div>
